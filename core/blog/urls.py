ac9d90111392e69ee334fd362e9359dbba13e285
mohsenpiri61 <mopiry@gmail.com>
2024-05-26 22:49:59 +0330

    mixins

--- core/blog/urls.py
+++ core/blog/urls.py
@@ -13,5 +13,9 @@ urlpatterns = [
     path('home/<int:pk>/', views.PageView.as_view()),
     path("post/", views.PostListView.as_view(), name="post-list"),
     path("post/<int:pk>/", views.PostDetailView.as_view(), name="post-detail"),
+    #path("post/create/", views.PostCreateFormView.as_view(), name="post-create"),
+    path("post/create/", views.PostCreateView.as_view(), name="post-create"),
+    path("post/<int:pk>/edit/", views.PostEditView.as_view(), name="post-edit"),
+    path("post/<int:pk>/delete/", views.PostDeleteView.as_view(), name="post-delete"),
 
 ]
--- core/blog/views.py
+++ core/blog/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import get_object_or_404, render
 from django.views.generic.base import TemplateView, RedirectView
 from .models import Post
-from django.views.generic import ListView, DetailView
+from django.views.generic import ListView, DetailView, FormView, CreateView, UpdateView, DeleteView
+from django.utils import timezone
+from .forms import PostForm
+from django.contrib.auth.mixins import (LoginRequiredMixin, PermissionRequiredMixin, )
 
 
 # override a TemplateView
@@ -29,13 +32,55 @@ class PageView(RedirectView):
         return super().get_redirect_url(*args, **kwargs)
 
 
-class PostListView(ListView):
-    # permission_required = "blog.view_post"
+class PostListView(PermissionRequiredMixin, ListView):
+    permission_required = "blog.view_post"
     # queryset = Post.objects.all()
+
     model = Post
     context_object_name = "obj_post"
-    paginate_by = 2
+    paginate_by = 4
     ordering = "-id"
     # def get_queryset(self):
     #     posts = Post.objects.filter(status=True)  # note that obj_post is called in html no posts
     #     return posts
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
+# creation of form using Formview
+"""
+class PostCreateFormView(FormView):
+    template_name = 'contact.html'
+    form_class = PostForm
+    success_url = '/blog/post/'
+
+    def form_valid(self, form):
+        form.save()
+        return super().form_valid(form)
+"""
+
+
+class PostCreateView(LoginRequiredMixin, CreateView):
+    model = Post
+    # fields = ['author', 'title', 'content','status', 'category', 'published_date']
+    form_class = PostForm
+    success_url = "/blog/post/"
+
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        return super().form_valid(form)
+
+
+class PostEditView(PermissionRequiredMixin, LoginRequiredMixin, UpdateView):
+    permission_required = 'blog.edit_post'
+    model = Post
+    form_class = PostForm
+    success_url = "/blog/post/"
+
+
+class PostDeleteView(PermissionRequiredMixin, LoginRequiredMixin, DeleteView):
+    permission_required = 'blog.delete_post'
+    model = Post
+    success_url = "/blog/post/"
